d55e252ef24d5c3500cde7820cdd47226c914608
Dzenly <alexchem@yandex.ru>
2018-01-16 12:53:50 +0700

    Saved local changes.

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -6,16 +6,18 @@ const { log, cfg } = require('./common.js');
 
 // require('winston').level = 'silly';
 
-log.info(`chdir: ${cfg.prjRoot}`);
-process.chdir(cfg.prjRoot);
+function buildAndDeploy() {
+  try {
+    log.info(`chdir: ${cfg.prjRoot}`);
+    process.chdir(cfg.prjRoot);
 
-try {
-  const res = gitUtils.pull({ pathPrefixes: ['deploy/cfg', 'deploy/common', 'deploy/dummy'] });
-  console.log(res);
-} catch (err) {
-  log.error(err);
+    const gitData = gitUtils.pull({ pathPrefixes: ['deploy/cfg', 'deploy/common', 'deploy/dummy'] });
+
+
+    console.log(res);
+  } catch (err) {
+    log.error(err);
+  }
 }
 
-// git rev-parse 3.3-test
-// git ls-remote origin -h refs/heads/3.3-test
-// ? git remote update
+buildAndDeploy();
